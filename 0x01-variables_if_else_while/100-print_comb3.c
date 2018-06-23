777b953b93414223b7d5160877862d21aebeb7aa
rydzign <ryan.rydzign@gmail.com>
2018-06-23 06:14:59 +0000

    advanced push

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * a program that prints all possible different combinations of two digits.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i, j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = i + 1; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (i != '8' || j != '9')
+{
+putchar(',');
+putchar(' ');
+count++;
+}
+}
+}
+putchar('\n');
+return (0);
+}
