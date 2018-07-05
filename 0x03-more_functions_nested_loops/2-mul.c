5420341786dfe0d1a3a6fbd1d180457a3bc85f4b
rydzign <ryan.rydzign@gmail.com>
2018-07-05 06:38:37 +0000

    comment edit

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
- * function that multiplies two integers.
- * Return: 0 for (Success)
+ * mul - function that multiplies two integers.
+ * @a: int 1
+ * @b: int 2
+ * Return: 0
  */
 
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
