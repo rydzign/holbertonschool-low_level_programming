439d1adda4103d7287b9908765da21809db88bf6
rydzign <ryan.rydzign@gmail.com>
2018-07-05 20:19:25 +0000

    deleted main

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
  * print_alphabet_x10 - print alphabet 10 times in lowercase, then newline
- * @c: string to print
- * Return: void
+ *
  */
 
 void print_alphabet_x10(void)
 {
 int i;
 char c;
+
 for (i = 0 ; i < 10 ; i++)
 {
 for (c = 'a' ; c <= 'z' ; c++)
