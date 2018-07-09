4ea4dc6febf431982b793612c5fe0ee6163a9b5e
rydzign <ryan.rydzign@gmail.com>
2018-07-09 05:16:48 +0000

    first push

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ *
+ *
+ */
+
+char *rot13(char *)
+{
+	int i, j;
+
+	while( j = i )
+{
+		do(( j = '0'; j >= 'N'; j <= 'M' ) && ( j = '0'; j >= 'n'; j <= 'm' ));
+		{
+
+	for(( i = '0'; i >= 'A'; i <= 'M' ) && ( i = '0'; i >= 'a'; i <= 'm' ));
+	{
+		if( i == i ){
+			return(int j);
+	}
+}
