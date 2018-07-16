927b100fb565ecd16cffb227670a4247bae9b67a
rydzign <ryan.rydzign@gmail.com>
2018-07-16 04:53:10 +0000

    3rd push

---
+++ 0x08-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns string length
+ * @s: pointer to string
+ *
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int p = 0;
+
+	while (*(s + p) != '\0')
+	{
+		p++;
+	}
+	return (p);
+}
