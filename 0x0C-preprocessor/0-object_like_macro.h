75d759448ffddc052087c32664523ef95a9ca27d
rydzign <ryan.rydzign@gmail.com>
2018-07-24 04:35:15 +0000

    added 0-

--- 0x0C-preprocessor/0-object_like_macro.h
+++ 0x0C-preprocessor/0-object_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef 0-OBJECT_LIKE_MACRO_H
+#define 0-OBJECT_LIKE_MACRO_H
 #define SIZE 1024
-#endif /* #ifndef OBJECT_LIKE_MACRO_H */
+#endif /* #ifndef 0-OBJECT_LIKE_MACRO_H */
