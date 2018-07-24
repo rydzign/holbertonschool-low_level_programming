02598af6459188febc8d300520999142296ca0e5
rydzign <ryan.rydzign@gmail.com>
2018-07-24 18:49:43 +0000

    edits again with ()

--- 0x0C-preprocessor/3-function_like_macro.h
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x < 0) ? -x : x)
+#define ABS(x) ((x < 0) ? (-x) : (x));
 #endif */ FUNCTION_LIKE_MACRO_H */
